56d4dc68ea72e691a7497718b9f5278c1a31d7ad
chaiwee <Fanchaiwee1234>
2016-07-14 11:55:47 +0800

    Order coffee

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,9 +22,25 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity=5;
+        int quantity=2;
         display(quantity);
-        displayPrice(quantity*5);
+        displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+        int quantity=3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity=1;
+        display(quantity);
     }
 
     /**
